0b96e7650d906eeedacd45bb2c51d4c9972cc620
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-09-17 12:54:50 +0530

    updated egress rule

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ resource "aws_security_group" "main" {
   description = var.sg_description
   vpc_id      = var.vpc_id
 
+  
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = merge( 
     local.common_tags,
     var.sg_tags,
